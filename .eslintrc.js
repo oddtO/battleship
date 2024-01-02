015e83aaf908cad4b6e9005585490b5352314311
x <x>
2024-01-02 14:37:01 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    commonjs: true,
+  },
+  // "\(@[^"/]*\)"
+  // eslint-\(plugin\|config\)-\w\+
+  extends: ["eslint:recommended", "eslint-config-prettier"],
+  plugins: ["eslint-plugin-html", "@html-eslint"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}", "*.config.js", ".stylelintrc.js"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+    {
+      files: ["*.html"],
+      parser: "@html-eslint/parser",
+      extends: ["plugin:@html-eslint/recommended"],
+      rules: { "@html-eslint/require-li-container": "off" },
+    },
+    {
+      files: ["*.test.js"],
+      rules: {
+        "no-undef": "off",
+        "no-unused-vars": "off",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "prefer-const": "error",
+  },
+};
