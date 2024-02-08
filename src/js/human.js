194893c1172088094397a26d6dfb07097c09749b
oddtO <121516833+oddtO@users.noreply.github.com>
2024-02-08 04:11:53 +0200

    dist

--- src/js/human.js
+++
@@ -1,8 +0,0 @@
-import { DOMHandler } from "./dom-handler";
-export class Human {
-  async makeMove() {
-    const [y, x] = await DOMHandler.askInput(this);
-
-    this.enemyGameboard.receiveAttack(y, x);
-  }
-}
--- src/js/random.js
+++
@@ -1,4 +0,0 @@
-export function RandomNum(min, max) {
-  const excludingMax = max - 1;
-  return Math.round(Math.random() * (excludingMax - min) + min);
-}
